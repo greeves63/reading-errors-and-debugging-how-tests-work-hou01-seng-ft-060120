f6f93b12eb74bf410106b748e0725160e85cc6f8
Gregory Reeves <gregoryreevesjr@gmail.com>
2020-05-28 05:32:25 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,26 @@
-# Add your variables here
+describe "./calculator.rb" do
+puts "Welcome to the calculator app"
+
+puts "First number:"
+first_number = gets.chomp.to_i
+
+puts "Second number:"
+second_number = gets.chomp.to_i
+
+puts "Operation:"
+operation = gets.chomp
+
+if operation == "+"
+  result = first_number + second_number
+elsif operation == "-"
+  result = first_number - second_number
+elsif operation == "/"
+  result = first_number.fdiv(second_number)
+elsif operation == "*"
+  result = first_number * second_number
+end
+  
+puts result
+
+
+end
